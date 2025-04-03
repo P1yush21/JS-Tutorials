c00ced63f4348e4393ae49904c844d3d5d6b0332
P1yush21 <piyushsharma21246@gmail.com>
2025-04-03 01:54:29 +0000

    03_conversionOperation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -23,5 +23,41 @@ let booleanIsloggedIn = Boolean(isLoggedIn)
 
 let someNumber= 33
 let stringNumber = String(someNumber)
-console. log (stringNumber) ;
-console. log(typeof stringNumber) ;
+// console. log (stringNumber) ;
+// console. log(typeof stringNumber) ;
+
+
+// ************************ OPERATIONS ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //2 raise to the power 2
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="Hello"
+let str2=" Piyush"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);  //12
+// console.log(1+"2");  //12
+// console.log("1"+2+2);  // 122
+// console.log(1+2+"2");  //32
+
+// console.log(+true);  //1
+// console.log(+"");  //0
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2+2 // poor code readability
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+  
