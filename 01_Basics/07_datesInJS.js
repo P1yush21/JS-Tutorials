472729163dc19a41ce7b53a152a5da7477ae077c
P1yush21 <piyushsharma21246@gmail.com>
2025-04-08 12:25:06 +0000

    07_datesInJS

--- 01_Basics/07_datesInJS.js
+++ 01_Basics/07_datesInJS.js
@@ -18,5 +18,21 @@ let myDate = new Date();
 // let myCreatedDate = new Date("2023-01-14")
 // console.log(myCreatedDate.toLocaleString()); //1/14/2023, 12:00:00 AM
 
-let myCreatedDate = new Date("01-14-2023")
-console.log(myCreatedDate.toLocaleString()); //1/14/2023, 12:00:00 AM
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString()); //1/14/2023, 12:00:00 AM
+
+// let myTimeStamp = new Date()
+// console.log(Date.now());
+// console.log(myTimeStamp.getTime());
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() +1);
+console.log(newDate.getDay());
+console.log(newDate.toLocaleString('default',{
+    weekday : "long", // tuesday
+    hour :"2-digit" // 12 PM
+}))
