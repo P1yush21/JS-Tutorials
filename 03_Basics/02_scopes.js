924ae40c394c3101be2c42b643b99a2965ec09f4
P1yush21 <piyushsharma21246@gmail.com>
2025-04-13 05:23:45 +0000

    02_Scopes.js

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,11 +1,51 @@
 var c = 388
 let a = 500
-if(true){
-    let a = 10
-    const b = 20 
-    console.log("INNER: ", a);
-}
+// if(true){
+//     let a = 10
+//     const b = 20 
+//     console.log("INNER: ", a);
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "piyush"
+    
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+
+// one()
+
+
+if(true){
+    const username = "piyush"
+    if(username === "piyush"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ++++++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5));
+function addone (num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
