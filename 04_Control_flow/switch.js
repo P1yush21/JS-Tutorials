581cc17f350c804e81e0921b1577311b80b01265
P1yush21 <piyushsharma21246@gmail.com>
2025-04-16 11:25:12 +0000

    Switch.js

---
+++ 04_Control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "january":
+        console.log("January");
+        break;
+    case "february":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
